49c0e0da3ade67f87d7ad31ab89d81f419c70f1a
Abhinav Chalise <82364981+AC17dollars@users.noreply.github.com>
2022-07-06 00:20:53 +0545

    removed a possible bug

--- dvd_ss.cpp
+++ dvd_ss.cpp
@@ -29,8 +29,8 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                window.close();
+//             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))         // may cause unwanted behaviour
+//                 window.close();
         }
         // movements
         shapePosition.x += xVelocity;
